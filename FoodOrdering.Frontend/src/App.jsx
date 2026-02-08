622442de8d29dadafbaf5cb9a0351830e8a7f8b8
utkarsh <raiutkarsh33@gmail.com>
2026-02-08 20:38:32 +0530

    add tailwind css setup

--- FoodOrdering.Frontend/src/App.jsx
+++ FoodOrdering.Frontend/src/App.jsx
@@ -14,6 +14,8 @@ function App() {
 
   return (
     <div>
+      <h1 className="text-3xl font-bold text-blue-600">Food Ordering App</h1>
+
       <div style={{ marginBottom: "20px" }}>
         <button onClick={() => setPage("restaurants")}>Restaurants</button>
         <button
